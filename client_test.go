89b050e624160fb17dcae943afc8023a63a3e8e0
Matt Layher <mdlayher@gmail.com>
2015-03-21 13:27:58 -0400

    client_test: add TestErrorError, to verify consistent Error.Error method output

--- client_test.go
+++ client_test.go
@@ -25,3 +25,53 @@ func TestNewClient(t *testing.T) {
 		}
 	}
 }
+
+// TestErrorError tests for consistent output from the Error.Error method.
+func TestErrorError(t *testing.T) {
+	var tests = []struct {
+		description string
+		code        int
+		eType       string
+		details     string
+		developer   string
+		result      string
+	}{
+		{
+			description: "only details",
+			code:        500,
+			eType:       "auth_failed",
+			details:     "authentication failed",
+			developer:   "",
+			result:      "500 [auth_failed]: authentication failed",
+		},
+		{
+			description: "only developer friendly",
+			code:        501,
+			eType:       "auth_failed",
+			details:     "",
+			developer:   "authentication failed due to server error",
+			result:      "501 [auth_failed]: authentication failed due to server error",
+		},
+		{
+			description: "both details and developer friendly",
+			code:        502,
+			eType:       "auth_failed",
+			details:     "authentication failed",
+			developer:   "authentication failed due to server error",
+			result:      "502 [auth_failed]: authentication failed due to server error",
+		},
+	}
+
+	for _, tt := range tests {
+		err := &Error{
+			Code:              tt.code,
+			Detail:            tt.details,
+			Type:              tt.eType,
+			DeveloperFriendly: tt.developer,
+		}
+
+		if res := err.Error(); res != tt.result {
+			t.Fatalf("unexpected result string for test %q: %q != %q", tt.description, res, tt.result)
+		}
+	}
+}
