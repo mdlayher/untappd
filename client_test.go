deccb2f764ebd56bdad1e58d5bc309bb2ca126be
Matt Layher <mdlayher@gmail.com>
2015-03-21 13:39:17 -0400

    client_test: add Test_timeUnitToDuration

--- client_test.go
+++ client_test.go
@@ -2,6 +2,7 @@ package untappd
 
 import (
 	"testing"
+	"time"
 )
 
 // TestNewClient tests for all possible errors which can occur during a call
@@ -72,6 +73,36 @@ func TestErrorError(t *testing.T) {
 
 		if res := err.Error(); res != tt.result {
 			t.Fatalf("unexpected result string for test %q: %q != %q", tt.description, res, tt.result)
+		}
+	}
+}
+
+// Test_timeUnitToDuration verifies that timeUnitToDuration provides proper
+// output for a variety of time number and measure values.
+func Test_timeUnitToDuration(t *testing.T) {
+	var tests = []struct {
+		description string
+		number      float64
+		measure     string
+		result      time.Duration
+	}{
+		{"0.05 milliseconds", 0.05, "milliseconds", time.Duration(5*time.Millisecond) / 100},
+		{"5 milliseconds", 5, "milliseconds", time.Duration(5 * time.Millisecond)},
+		{"500 milliseconds", 500, "milliseconds", time.Duration(500 * time.Millisecond)},
+		{"0.5 seconds", 0.5, "seconds", time.Duration(500 * time.Millisecond)},
+		{"1 seconds", 1, "seconds", time.Duration(1 * time.Second)},
+		{"10 seconds", 10, "seconds", time.Duration(10 * time.Second)},
+		{"0.5 minutes", 0.5, "minutes", time.Duration(30 * time.Second)},
+		{"1 minutes", 1, "minutes", time.Duration(1 * time.Minute)},
+		{"2 minutes", 2, "minutes", time.Duration(2 * time.Minute)},
+		{"invalid: 100 hours", 100, "hours", time.Duration(0)},
+		{"invalid: 10 days", 10, "days", time.Duration(0)},
+		{"invalid: 1 lightyears", 1, "lightyears", time.Duration(0)},
+	}
+
+	for _, tt := range tests {
+		if dur := timeUnitToDuration(tt.number, tt.measure); dur != tt.result {
+			t.Fatalf("unexpected duration for test %q: %v != %v", tt.description, dur, tt.result)
 		}
 	}
 }
