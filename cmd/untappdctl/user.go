5467a55aa0c3adfe589ba5bebf5d52e891c3fa50
Matt Layher <mdlayher@gmail.com>
2015-04-06 00:06:18 -0400

    cmd/untappdctl/user: add basic badge printing

--- cmd/untappdctl/user.go
+++ cmd/untappdctl/user.go
@@ -74,10 +74,8 @@ func userBadgesCommand(offsetFlag cli.IntFlag, limitFlag cli.IntFlag) cli.Comman
 				log.Fatal(err)
 			}
 
-			// BUG(mdlayher): add pretty printing for Badge information.
-			for _, b := range badges {
-				fmt.Println(b)
-			}
+			// Print out badges in human-readable format
+			printBadges(badges)
 		},
 	}
 }
@@ -213,10 +211,46 @@ func userWishListCommand(offsetFlag cli.IntFlag, limitFlag cli.IntFlag, sortFlag
 	}
 }
 
+// printBadges turns a slice of *untappd.Badge structs into a human-friendly
+// output format, and prints it to stdout.
+func printBadges(badges []*untappd.Badge) {
+	tw := tabWriter()
+
+	// Print field header
+	fmt.Fprintln(tw, "ID\tName\tEarned\tCheckinID")
+
+	// Function to be invoked for each badge and badge level
+	printFn := func(b *untappd.Badge) {
+		y, m, d := b.Earned.Date()
+
+		fmt.Fprintf(tw, "%d\t%s\t%s\t%d\n",
+			b.ID,
+			b.Name,
+			fmt.Sprintf("%04d-%02d-%02d", y, m, d),
+			b.CheckinID,
+		)
+	}
+
+	// Print out each badge
+	for _, b := range badges {
+		printFn(b)
+
+		// Print out each badge level
+		for _, bb := range b.Levels {
+			printFn(bb)
+		}
+	}
+
+	// Flush buffered output
+	if err := tw.Flush(); err != nil {
+		log.Fatal(err)
+	}
+}
+
 // printBeers turns a slice of *untappd.Beer structs into a human-friendly
 // output format, and prints it to stdout.
 func printBeers(beers []*untappd.Beer) {
-	tw := tabwriter.NewWriter(os.Stdout, 0, 8, 2, '\t', 0)
+	tw := tabWriter()
 
 	// Print field header
 	fmt.Fprintln(tw, "ID\tName\tBrewery\tStyle\tABV\tIBU")
@@ -237,4 +271,10 @@ func printBeers(beers []*untappd.Beer) {
 	if err := tw.Flush(); err != nil {
 		log.Fatal(err)
 	}
+}
+
+// tabWriter returns a *tabwriter.Writer appropriately configured
+// for tabular output.
+func tabWriter() *tabwriter.Writer {
+	return tabwriter.NewWriter(os.Stdout, 0, 8, 2, '\t', 0)
 }
