3cc0949e974d7033ff9854903e122024dceb4258
Matt Layher <mdlayher@gmail.com>
2015-04-21 20:58:05 -0400

    auth{,_test}: initial commit of AuthHandler type, which allows for easy authentication with Untappd APIv4

--- auth.go
+++ auth.go
@@ -1,7 +1,188 @@
 package untappd
 
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"net/url"
+	"strings"
+)
+
+const (
+	// untappdOAuthAuthenticate is a URL pattern which is interpolated with
+	// an Untappd APIv4 client ID and redirect URL.  The resulting URL can
+	// be returned to clients to begin the authentication process.
+	untappdOAuthAuthenticate = "https://untappd.com/oauth/authenticate/?client_id=%s&response_type=code&redirect_url=%s"
+
+	// untappdOAuthAuthorize is a URL pattern which is interpolated with
+	// an Untappd APIv4 client ID, client secret, and redirect URL.
+	// The resulting URL is requested by AuthHandler.ServeHTTP, generating
+	// an Access Token for client consumption.
+	untappdOAuthAuthorize = "https://untappd.com/oauth/authorize/?client_id=%s&client_secret=%s&response_type=code&redirect_url=%s"
+)
+
 // AuthService is a "service" which allows access to API methods which require
 // authentication.
 type AuthService struct {
 	client *Client
+}
+
+// AuthHandler implements http.Handler, and provides a simple process for
+// authenticating users using OAuth with Untappd APIv4.
+type AuthHandler struct {
+	clientID     string
+	clientSecret string
+	redirectURL  *url.URL
+	oAuthURL     *url.URL
+	handler      TokenHandlerFunc
+	client       *http.Client
+}
+
+// TokenHandlerFunc is a function which is invoked at the end of a successful
+// AuthHandler authentication process.  The token generated during the process is
+// provided via the token parameter, and the HTTP request and response writers are
+// available for further HTTP processing.
+type TokenHandlerFunc func(token string, w http.ResponseWriter, r *http.Request)
+
+// defaultTokenFn is the default implementation of TokenHandlerFunc, and is used
+// automatically by NewAuthHandler, unless a custom TokenHandlerFunc is provided.
+// This function simply prints the token to the HTTP response writer.
+var defaultTokenFn = func(token string, w http.ResponseWriter, r *http.Request) {
+	if _, err := w.Write([]byte(token)); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+// NewAuthHandler creates a http.Handler which can be used to easily authenticate
+// a user using the Server Side Authentication process, documented here:
+// https://untappd.com/api/docs#authentication.
+//
+// The first return parameter is the http.Handler described above.  The second is
+// a URL which should be provided to a user, so that they can begin the
+// authentication flow.  The third contains any errors which may have occurred
+// during setup.
+//
+// The client ID, client secret, and redirectURL parameters are mandatory.
+//
+// The TokenHandlerFunc parameter can be used to provide a custom handler which
+// contains an access token, and HTTP request and response writers, for further
+// processing. The TokenHandlerFunc is only called upon successful authentication.
+// Otherwise, an HTTP error is returned to the client.  If no TokenHandlerFunc is
+// provided, a default one which writes the token to the HTTP response body will
+// be used.
+//
+// The http.Client parameter can be used to provide a custom http.Client which
+// obeys timeouts, etc.  This client is used to communicate with an upstream
+// OAuth authentication server.  If no http.Client is provided, http.DefaultClient
+// will be used.
+func NewAuthHandler(clientID string, clientSecret string, redirectURL string, fn TokenHandlerFunc, client *http.Client) (*AuthHandler, *url.URL, error) {
+	// Disallow empty ID and secret
+	if clientID == "" {
+		return nil, nil, ErrNoClientID
+	}
+	if clientSecret == "" {
+		return nil, nil, ErrNoClientSecret
+	}
+
+	// Validate user redirect URL
+	ru, err := url.Parse(redirectURL)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	// Validate client authentication URL
+	cu, err := url.Parse(fmt.Sprintf(
+		untappdOAuthAuthenticate,
+		clientID,
+		ru.String(),
+	))
+	if err != nil {
+		return nil, nil, err
+	}
+
+	// Validate OAuth URL
+	ou, err := url.Parse(fmt.Sprintf(
+		untappdOAuthAuthorize,
+		clientID,
+		clientSecret,
+		ru.String(),
+	))
+
+	// If no token handler is set, use default
+	if fn == nil {
+		fn = defaultTokenFn
+	}
+
+	// If no client set, use http.DefaultClient
+	if client == nil {
+		client = http.DefaultClient
+	}
+
+	return &AuthHandler{
+		clientID:     clientID,
+		clientSecret: clientSecret,
+		redirectURL:  ru,
+		oAuthURL:     ou,
+		handler:      fn,
+		client:       client,
+	}, cu, nil
+}
+
+// ServeHTTP implements http.Handler, and provides a simple http.Handler which
+// can properly authenticate using the Server Side Authentication method outlined
+// in Untappd documentation: https://untappd.com/api/docs#authentication.
+func (a *AuthHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	// Verify correct HTTP method
+	if r.Method != "GET" {
+		http.Error(w, "only GET requests are allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	// Verify non-empty code parameter
+	code := r.URL.Query().Get("code")
+	if code == "" {
+		http.Error(w, "no 'code' GET parameter", http.StatusBadRequest)
+		return
+	}
+
+	// Perform HTTP GET request to retrieve token using the
+	// code provided from query parameter
+	res, err := a.client.Get(a.oAuthURL.String() + "&code=" + code)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// Verify authentication server did not return an error
+	if c := res.StatusCode; c > 299 || c < 200 {
+		http.Error(w, fmt.Sprintf("authentication server error: HTTP %03d", c), http.StatusBadGateway)
+		return
+	}
+
+	// Verify authentication server returned JSON
+	if !strings.Contains(res.Header.Get("Content-Type"), jsonContentType) {
+		http.Error(w, "authentication server sent non-JSON content", http.StatusBadGateway)
+		return
+	}
+
+	// Temporary struct for JSON body
+	var v struct {
+		Response struct {
+			AccessToken string `json:"access_token"`
+		} `json:"response"`
+	}
+
+	// Decode JSON body to retrieve token
+	if err := json.NewDecoder(res.Body).Decode(&v); err != nil {
+		http.Error(w, err.Error(), http.StatusBadGateway)
+		return
+	}
+	if err := res.Body.Close(); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// Invoke TokenHandlerFunc to provide easy access to the generated token,
+	// so the client can do whatever they please with it
+	a.handler(v.Response.AccessToken, w, r)
 }
---
+++ auth_test.go
@@ -0,0 +1,265 @@
+package untappd
+
+import (
+	"errors"
+	"log"
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+	"reflect"
+	"testing"
+)
+
+// TestNewAuthHandler verifies that NewAuthHandler returns appropriate errors
+// for various types of input parameters.
+func TestNewAuthHandler(t *testing.T) {
+	const badURL = "http://%a0.com"
+
+	var tests = []struct {
+		description  string
+		clientID     string
+		clientSecret string
+		redirectURL  string
+		err          error
+	}{
+		{
+			description: "no client ID or client secret",
+			err:         ErrNoClientID,
+		},
+		{
+			description:  "no client ID",
+			clientSecret: "bar",
+			err:          ErrNoClientID,
+		},
+		{
+			description: "no client secret",
+			clientID:    "foo",
+			err:         ErrNoClientSecret,
+		},
+		{
+			description:  "bad redirect URL",
+			clientID:     "foo",
+			clientSecret: "bar",
+			redirectURL:  badURL,
+			err: &url.Error{
+				Op:  "parse",
+				URL: badURL,
+			},
+		},
+		{
+			description:  "ok",
+			clientID:     "foo",
+			clientSecret: "bar",
+			redirectURL:  "http://foo.com",
+		},
+	}
+
+	for _, tt := range tests {
+		if _, _, err := NewAuthHandler(tt.clientID, tt.clientSecret, tt.redirectURL, nil, nil); err != tt.err {
+			// Special case: check for matching type *url.Error
+			if reflect.TypeOf(err) == reflect.TypeOf(tt.err) {
+				continue
+			}
+
+			t.Fatalf("unexpected error for test %q: %v != %v", tt.description, err, tt.err)
+		}
+	}
+}
+
+// TestAuthHandlerServeHTTPBadMethod verifies that AuthHandler returns a
+// HTTP 405 on non-GET method.
+func TestAuthHandlerServeHTTPBadMethod(t *testing.T) {
+	url, done := testAuthHandler(t, "http://foo.com/", "", nil)
+	defer done()
+
+	res, err := http.Post(url, "", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if got, want := res.StatusCode, http.StatusMethodNotAllowed; got != want {
+		log.Fatalf("unexpected HTTP status code: %d != %d", got, want)
+	}
+}
+
+// TestAuthHandlerServeHTTPBadMethod verifies that AuthHandler returns a
+// HTTP 401 if no code parameter is passed via query string.
+func TestAuthHandlerServeHTTPNoCodeParameter(t *testing.T) {
+	url, done := testAuthHandler(t, "http://foo.com", "", nil)
+	defer done()
+
+	res, err := http.Get(url)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if got, want := res.StatusCode, http.StatusBadRequest; got != want {
+		log.Fatalf("unexpected HTTP status code: %d != %d", got, want)
+	}
+}
+
+// TestAuthHandlerServeHTTPOAuthInternalServerError verifies that AuthHandler
+// returns a HTTP 502 if the upstream server returns a non-200 status code.
+func TestAuthHandlerServeHTTPOAuthInternalServerError(t *testing.T) {
+	testOAuthBadGateway(t, func(t *testing.T, w http.ResponseWriter, r *http.Request) {
+		http.Error(w, "internal server error", http.StatusInternalServerError)
+	})
+}
+
+// TestAuthHandlerServeHTTPOAuthNotJSON verifies that AuthHandler returns a HTTP 502
+// if the upstream server returns a non-JSON Content-Type header.
+func TestAuthHandlerServeHTTPOAuthNotJSON(t *testing.T) {
+	testOAuthBadGateway(t, func(t *testing.T, w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/plain")
+		w.Write([]byte("hello world"))
+	})
+}
+
+// TestAuthHandlerServeHTTPOAuthBadJSON verifies that AuthHandler returns a HTTP
+// 502 if the upstream server returns broken JSON.
+func TestAuthHandlerServeHTTPOAuthBadJSON(t *testing.T) {
+	testOAuthBadGateway(t, func(t *testing.T, w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", jsonContentType)
+		w.Write([]byte("{"))
+	})
+}
+
+// TestAuthHandlerServeHTTPOK verifies that AuthHandler can complete an
+// entire mock authentication cycle, and return the correct final token upon
+// successful authentication.
+func TestAuthHandlerServeHTTPOK(t *testing.T) {
+	expectedToken := "ABCDEF0123456789"
+
+	oauthHost, done := testOAuthServer(t, func(t *testing.T, w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", jsonContentType)
+		w.Write([]byte(`{"response":{"access_token":"` + expectedToken + `"}}`))
+	})
+	defer done()
+
+	var accessToken string
+	tokenFn := func(token string, w http.ResponseWriter, r *http.Request) {
+		accessToken = token
+	}
+
+	url, done2 := testAuthHandler(t, "http://foo.com", oauthHost, tokenFn)
+	defer done2()
+
+	res, err := http.Get(url + "?code=foo")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if got, want := res.StatusCode, http.StatusOK; got != want {
+		t.Fatalf("unexpected HTTP status code: %d != %d", got, want)
+	}
+
+	if got, want := accessToken, expectedToken; got != want {
+		t.Fatalf("unexpected access token: %q != %q", got, want)
+	}
+}
+
+// errBrokenWriter is always returned by brokenResponseWriter.Write.
+var errBrokenWriter = errors.New("broken writer")
+
+// brokenResponseWriter is a http.ResponseWriter which always returns
+// an error when its Write method is called.
+type brokenResponseWriter struct {
+	*httptest.ResponseRecorder
+}
+
+// Write implements a broken Write method for a brokenResponseWriter.
+func (w *brokenResponseWriter) Write(b []byte) (int, error) {
+	return 0, errBrokenWriter
+}
+
+// Test_defaultTokenFnBadWriter verifies that defaultTokenFn returns an
+// internal server error if it is unable to write a response body.
+func Test_defaultTokenFnBadWriter(t *testing.T) {
+	w := &brokenResponseWriter{
+		ResponseRecorder: httptest.NewRecorder(),
+	}
+
+	defaultTokenFn("", w, nil)
+
+	if got, want := w.Code, http.StatusInternalServerError; got != want {
+		t.Fatalf("unexpected HTTP status code: %d != %d", got != want)
+	}
+}
+
+// Test_defaultTokenFnOK verifies that defaultTokenFn returns a valid token
+// if it is unable to write a response body.
+func Test_defaultTokenFnOK(t *testing.T) {
+	for _, tok := range []string{"foo", "bar", "baz"} {
+		rec := httptest.NewRecorder()
+
+		defaultTokenFn(tok, rec, nil)
+
+		if b := rec.Body.String(); b != tok {
+			t.Fatalf("unexpected response body: %q != %q", b, tok)
+		}
+	}
+}
+
+// testAuthHandler creates a mocked AuthHandler which points at a httptest server,
+// and returns that server's URL and a function to shut it down.
+func testAuthHandler(t *testing.T, redirectURL string, oauthHost string, fn TokenHandlerFunc) (string, func()) {
+	h, _, err := NewAuthHandler(
+		"foo",
+		"bar",
+		redirectURL,
+		fn,
+		nil,
+	)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	h.oAuthURL.Scheme = "http"
+	h.oAuthURL.Host = oauthHost
+
+	srv := httptest.NewServer(h)
+	return srv.URL, func() {
+		srv.Close()
+	}
+}
+
+// testAuthHandler creates a httptest server which mocks an upstream OAuth server,
+// and which invokes an input closure, returning that server's host and a function
+// to shut it down.
+func testOAuthServer(t *testing.T, fn func(t *testing.T, w http.ResponseWriter, r *http.Request)) (string, func()) {
+	srv := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", jsonContentType)
+
+		if fn != nil {
+			fn(t, w, r)
+		}
+	}))
+
+	u, err := url.Parse(srv.URL)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return u.Host, func() {
+		srv.Close()
+	}
+}
+
+// testOAuthBadGateway handles common setup procedures for tests which check
+// for a HTTP 503 Bad Gateway error.
+func testOAuthBadGateway(t *testing.T, fn func(t *testing.T, w http.ResponseWriter, r *http.Request)) {
+	oauthHost, done := testOAuthServer(t, fn)
+	defer done()
+
+	url, done2 := testAuthHandler(t, "http://foo.com", oauthHost, nil)
+	defer done2()
+
+	res, err := http.Get(url + "?code=foo")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if got, want := res.StatusCode, http.StatusBadGateway; got != want {
+		t.Fatalf("unexpected HTTP status code: %d != %d", got, want)
+	}
+}
